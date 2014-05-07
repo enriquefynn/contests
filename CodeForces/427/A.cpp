972e823d666c8b2b98675f68725769d966bee1ea
Fynn <enriquefynn@gmail.com>
2014-05-07 19:27:54 -0300

    adhoc

---
+++ CodeForces/427/A.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#define EPS 0.0000000000000001
+#define fori(N) for(i = 0; i < N; ++i)
+#define forid(N) for(int i = 0; i < N; ++i)
+#define forj(N) for(j = 0; j < N; ++j)
+#define forjd(N) for(int j = 0; j < N; ++j)
+
+using namespace std;
+
+int gcd(int a, int b) {return (b = 0 ? a : gcd(b, a%b));}
+int lcm(int a, int b) {return (a * (b / gcd(a, b))); }
+
+struct pto
+{
+	double x;
+	double y;
+	pto(){}
+	pto(double _x, double _y){ x = _x; y = _y;}
+};
+
+int main()
+{
+    int n, c = 0, cri = 0;
+    cin >> n;
+    while(n--)
+    {
+        int p;
+        cin >> p;
+        if (p == -1 && c <= 0)
+            ++cri;
+        c+=p;
+        if (c < 0)
+            c = 0;
+    }
+    cout << cri << endl;
+}
