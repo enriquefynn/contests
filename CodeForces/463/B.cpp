e3e631cc27de6cf5dec3bdb709b7b21276fda4c2
Fynn <enriquefynn@gmail.com>
2014-09-01 14:22:59 -0300

    ad hoc

---
+++ CodeForces/463/B.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <map>
+#include <set>
+#define EPS 0.0000000000000001
+#define fori(N) for(i = 0; i < N; ++i)
+#define forid(N) for(int i = 0; i < N; ++i)
+#define forj(N) for(j = 0; j < N; ++j)
+#define forjd(N) for(int j = 0; j < N; ++j)
+typedef unsigned long long ull;
+typedef long long ll;
+
+using namespace std;
+
+int gcd(int a, int b) {return (b = 0 ? a : gcd(b, a%b));}
+int lcm(int a, int b) {return (a * (b / gcd(a, b))); }
+
+struct pto
+{
+	double x;
+	double y;
+	pto(){}
+	pto(double _x, double _y){ x = _x; y = _y;}
+};
+
+int main()
+{
+    int n, p, q = 0;
+    cin >> n;
+    int mind = INT_MAX;
+    forid(n)
+    {
+        cin >> p;
+        mind = min((q - p), mind);
+    }
+    if (mind >= 0)
+        cout << 0;
+    else
+        cout << -mind;
+    cout << endl;
+}
